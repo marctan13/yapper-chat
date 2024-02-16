6875549bea280e065d56c49406dd8e43dd85e8bb
Keno Reyes <keno.reyes.cohort238@gmail.com>
2024-02-16 15:42:20 -0800

    sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,46 +1,44 @@
-import React from "react";
-import SignOut from "./SignOut";
-import { auth } from "../firebase.js";
-import "bulma/css/bulma.css";
-import Preview from "./Preview.jsx";
+// import { useState } from "react";
+// import { auth } from "../firebase.js";
 import { Link } from "react-router-dom";
+import "bulma/css/bulma.css";
+import SignOut from "./SignOut.jsx";
+import Navbar from './SideBar/Navbar.jsx'
+import ChannelPreview from "./SideBar/ChannelPreview.jsx";
+// import DmPreview from './Sidebar/DmPreview.jsx'
 
 function Sidebar() {
+  //*SETS SIDEBAR PREVIEW TO DM OR CHANNEL*
+  // const [showChannelPreview, setShowChannelPreview] = useState(true)
+
+  // const togglePreview = () => {
+  //   setShowChannelPreview(prevState => !prevState)
+  // }
+
   return (
     <div className="sidebar">
-      <div className="nav-title">
-        <span className="logo">🗣️</span>
-        <span className="title">Yapper Chat</span>
-      </div>
-
-      {/* Chat previews */}
-      <div className="preview">
-        <div className="toggle">
-          <span className="toggle-label">DMs</span>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round"></span>
-          </label>
-          <span className="toggle-label">Channel</span>
+      <Navbar />
+      <section>
+        {/*To switch between gc and private dm*/}
+        {/* {showChannelPreview ? <ChannelPreview /> : <DmPreview />} */}
+      </section>
+      {/*temp preview placement for layout*/}
+      <ChannelPreview />
+      <ChannelPreview />
+      <ChannelPreview />
+      <ChannelPreview />
+      <div className="footer">
+        <div className="user">
+          <Link to="/settings">
+            <img className="userAvatar" src="/cup.jpg" />
+              </Link>
+                {/* <img src={auth.currentUser.photoURL} alt="" />
+                <span>{auth.currentUser.displayName}</span> */}
+                <SignOut />
+            </div>
+          <div className="chats"></div> 
         </div>
-        <button className="newChat">+ Create new Message</button>
-        <Preview />
-        <Preview />
-        <Preview />
-        <Preview />
-        <Preview />
       </div>
-
-      <div className="user">
-        <Link to="/settings">
-          <img className="userAvatar" src="/cup.jpg" />
-        </Link>
-        {/* <img src={auth.currentUser.photoURL} alt="" />
-        <span>{auth.currentUser.displayName}</span> */}
-        <SignOut />
-      </div>
-      <div className="chats"></div>
-    </div>
   );
 }
 
